7bd5e509296467941fa064ca59c4ec95800a37bd
negroiso <33251215a@gmail.com>
2025-07-24 13:11:06 -0500

    Uploading Files

---
+++ Sharp Vam Tools/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Sharp VaM Tools by Boss963")]
+[assembly: AssemblyDescription("A software developed to manage Virt-a-Mate vars (download dependencies, scan vars for malware, analayze vars & more)")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("F95")]
+[assembly: AssemblyProduct("Sharp VaM Tools")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("F95")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("99666333-ca9a-4c66-99a1-333a1e73e333")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
